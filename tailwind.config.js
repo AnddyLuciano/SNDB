240d1ece720c59e34e724141f36ebfe2287d5b4e
Anddy Luciano <luciano.ratsimbazafindramanana@gmail.com>
2024-04-05 08:26:13 +0200

    config: update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,tsx,ts,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{html,tsx,ts,js,jsx}", "./package/**/*.{html,tsx,ts,js,jsx}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#2a68ff",
+                secondary: "#e1e4e8",
+                body: "#6f6f6f",
+            },
+            boxShadow: {
+                base: "#f7f8f9",
+            },
+        },
+    },
+    plugins: [],
+};
